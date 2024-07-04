ea23d3b771e726b11c2803649bfb092ec361e5f7
giovanna alves <ruthjoaoester2@gmail.com>
2024-07-04 09:39:33 -0300

    adiciona os arquivos finalizados

---
+++ funcoes - calculadora.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+float somar(float + float )
+{
+    retur x + y
+}
+
+float subtrair(float - float )
+{
+    retur x - y
+}
+
+float multiplicar(float * float )
+{
+    retur x * y
+}
+
+float dividir(float / float )
+{
+    retur x / y
+}
+
+
+
+
+
+int main()
+{
+    float numero1 = 5;
+    float numero2 = 15;
+    string operacao = "+"
+
+    float resultado = 0;
+
+    if (operacao == "+")
+    {
+        resultado = somar (numero1, numero2)
+    }else if (operacao == "-")
+    {
+        resultado = subtrair (numero1, numero2)
+    }else if (operacao == "*"){
+        resultado = multiplicar(numero1,numero2)
+    }else if (operacao == "/"){
+        resultado = dividir(numero1, numero2);
+    }
+    
+    
+    return 0;
+}
